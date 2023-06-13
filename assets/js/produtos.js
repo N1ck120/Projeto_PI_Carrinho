a17b31723aeb9f24f9f1dd6d8a00fc7500c92f78
pedrohrqphp <pedrohrq2005@gmail.com>
2023-06-13 03:15:51 -0300

    Produtos com funcionalidade de carrinho finalizadas

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -15,7 +15,7 @@ function dbprodutos(){
       {id:4, nome:"Acionador Manual BIX3000 Ascael", desc:"Acionador", preco:590, img:"assets/img/Imgs_Produtos/AcioandorManualConvencionalBIX3000.png"},
 
       {id:5, nome:"Detector de Fumaça Endereçável Ascael", desc:"Detector", preco:430, img:"assets/img/Imgs_Produtos/DetectorFumacaEnderecavel.png"},
-      {id:6, nome:"Central Alarme de Incêndio CAX-i Ascael", desc:"Alarme", preco:690, img:"assets/img/produtos/CAX-i.png"},
+      {id:6, nome:"Central Alarme de Incêndio CAX-i Ascael", desc:"Alarme", preco:690, img:"assets/img/Imgs_Produtos/CAX-i.png"},
       {id:7, nome:"Central de Iluminação de Emergência Ascael", desc:"Iluminação", preco:490, img:"assets/img/produtos/CentralIluminacaoEmergencia.png"},
       {id:8, nome:"Central Endereçável Ascael", desc:"Endereçável", preco:710, img:"assets/img/produtos/CentralEnderecavel.png"},
       {id:9, nome:"Módulo Horus 3000 Ascael", desc:"Módulo", preco:645, img:"assets/img/Imgs_Produtos/ModuloHorusMes3000.png"},
@@ -59,27 +59,25 @@ function carregaprod(){
   document.getElementById("prod2").innerHTML = prod[1].nome;
   document.getElementById("prod3").innerHTML = prod[2].nome;
   document.getElementById("prod4").innerHTML = prod[3].nome;
-/*
   document.getElementById("prod5").innerHTML = prod[4].nome;
   document.getElementById("prod6").innerHTML = prod[5].nome;
   document.getElementById("prod7").innerHTML = prod[6].nome;
   document.getElementById("prod8").innerHTML = prod[7].nome;
   document.getElementById("prod9").innerHTML = prod[8].nome;
   document.getElementById("prod10").innerHTML = prod[9].nome;
-  document.getElementById("prod11").innerHTML = prod[10].nome;*/
+  document.getElementById("prod11").innerHTML = prod[10].nome;
 
   document.getElementById("prec1").innerHTML = "R$"+prod[0].preco;
   document.getElementById("prec2").innerHTML = "R$"+prod[1].preco;
   document.getElementById("prec3").innerHTML = "R$"+prod[2].preco;
   document.getElementById("prec4").innerHTML = "R$"+prod[3].preco;
-/*
   document.getElementById("prec5").innerHTML = "R$"+prod[4].preco;
   document.getElementById("prec6").innerHTML = "R$"+prod[5].preco;
   document.getElementById("prec7").innerHTML = "R$"+prod[6].preco;
   document.getElementById("prec8").innerHTML = "R$"+prod[7].preco;
   document.getElementById("prec9").innerHTML = "R$"+prod[8].preco;
   document.getElementById("prec10").innerHTML = "R$"+prod[9].preco;
-  document.getElementById("prec11").innerHTML = "R$"+prod[10].preco;*/
+  document.getElementById("prec11").innerHTML = "R$"+prod[10].preco;
 
   var imagem = document.getElementById('img1');
   imagem.src = prod[0].img;
@@ -89,7 +87,6 @@ function carregaprod(){
   imagem.src = prod[2].img;
   var imagem = document.getElementById('img4');
   imagem.src = prod[3].img;
-  /*
   var imagem = document.getElementById('img5');
   imagem.src = prod[4].img;
   var imagem = document.getElementById('img6');
@@ -103,7 +100,7 @@ function carregaprod(){
   var imagem = document.getElementById('img10');
   imagem.src = prod[9].img;
   var imagem = document.getElementById('img11');
-  imagem.src = prod[10].img;*/
+  imagem.src = prod[10].img;
 }
 
 
