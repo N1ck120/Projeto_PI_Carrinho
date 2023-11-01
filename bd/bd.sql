281c0189fdfeeed8e380ea3f82bbc45465f3e938
_ <85962372+N1ck120@users.noreply.github.com>
2023-11-01 01:44:47 +0000

    Create bd.sql

---
+++ bd/bd.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE BO_Solucoes;
+USE BO_Solucoes;
+
+CREATE TABLE User(
+    Id_User INT         NOT NULL	AUTO_INCREMENT,
+    Nome    VARCHAR(90) NOT NULL,
+    Email   VARCHAR(90) NOT NULL,
+    Senha   VARCHAR(90) NOT NULL,
+    
+    CONSTRAINT PK_User PRIMARY KEY(id_user)
+);
+
+CREATE TABLE Produto(
+    Id_Produto      INT         NOT NULL	AUTO_INCREMENT,
+    Img_Produto     BLOB        NOT NULL,
+    Nm_Produto      VARCHAR(90) NOT NULL,
+    Preco_Produto   DECIMAL(10,2),
+    
+    CONSTRAINT PK_Produto PRIMARY KEY(id_produto)
+);
+
+CREATE TABLE Carrinho(
+	Id_Carrinho 		INT				NOT NULL	AUTO_INCREMENT,
+    Id_User      		INT				NOT NULL,
+    Valor_Carrinho		DECIMAL(10,2)	NOT NULL,
+    
+    CONSTRAINT PK_Carrinho PRIMARY KEY(Id_Carrinho)
+);
+
+
+
+SELECT * FROM User;
+SELECT * FROM Produto;
+SELECT * FROM Carrinho;
+SELECT * FROM ProdutoCarrinho;
+
+
+DROP DATABASE BO_Solucoes;
