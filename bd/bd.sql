7f425dab11a439607b56a023f4af2c40bfea7f8d
pedrohrqphp <pedrohrq2005@gmail.com>
2023-11-16 23:25:35 -0300

    Produtos

--- bd/bd.sql
+++ bd/bd.sql
@@ -2,38 +2,49 @@ CREATE DATABASE BO_Solucoes;
 USE BO_Solucoes;
 
 CREATE TABLE User(
-    Id_User INT         NOT NULL	AUTO_INCREMENT,
+	Id_User INT         NOT NULL    AUTO_INCREMENT,
     Nome    VARCHAR(90) NOT NULL,
     Email   VARCHAR(90) NOT NULL,
     Senha   VARCHAR(90) NOT NULL,
     
-    CONSTRAINT PK_User PRIMARY KEY(id_user)
+    CONSTRAINT PK_User PRIMARY KEY(Id_User)
 );
 
 CREATE TABLE Produto(
-    Id_Produto      INT         NOT NULL	AUTO_INCREMENT,
-    Img_Produto     BLOB        NOT NULL,
-    Nm_Produto      VARCHAR(90) NOT NULL,
+    Id_Produto      INT             NOT NULL    AUTO_INCREMENT,
+    Nm_Produto      VARCHAR(90)     NOT NULL,
     Preco_Produto   DECIMAL(10,2),
     
-    CONSTRAINT PK_Produto PRIMARY KEY(id_produto)
+    CONSTRAINT PK_Produto PRIMARY KEY(Id_Produto)
 );
 
 CREATE TABLE Carrinho(
-	Id_Carrinho 		INT				NOT NULL	AUTO_INCREMENT,
-    Id_User      		INT				NOT NULL,
-    Valor_Carrinho		DECIMAL(10,2)	NOT NULL,
+    Id_Carrinho			INT					NOT NULL    AUTO_INCREMENT,
+    Id_User             INT                	NOT NULL,
+    Valor_Carrinho      DECIMAL(10,2)    	NOT NULL,
     
     CONSTRAINT PK_Carrinho PRIMARY KEY(Id_Carrinho),
     CONSTRAINT FK_User FOREIGN KEY(Id_User) REFERENCES User(Id_User)
 );
 
-
+INSERT INTO Produto(Nm_Produto, Preco_Produto) VALUES
+	('Central Amplificador Ascael', 600.00),
+    ('Alarme de Incêndio Ascael', 500.00),
+    
+    ('Boroeira Endereçável Ascael', 440.00),
+    ('Acionador Manual BIX3000 Ascael', 590.00),
+    
+    ('Detector de Fumaça Endereçável Ascael', 430.00),
+    ('Central Alarme de Incêndio CAX-i Ascael', 690.00),
+    ('Central de Iluminação de Emergência', 490.00),
+    ('Central Endereçável Ascael', 710.00),
+    ('Módulo Horus 3000 Ascael', 645.00),
+    ('Central de Endereçmento Individual Ascael', 435.00),
+    
+    ('Sirene Audiovisual Ascael', 475.00);
 
 SELECT * FROM User;
 SELECT * FROM Produto;
 SELECT * FROM Carrinho;
-SELECT * FROM ProdutoCarrinho;
-
 
 DROP DATABASE BO_Solucoes;
